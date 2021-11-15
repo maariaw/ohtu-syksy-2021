95744a405f7c9db409e6898490f1a0a459cad28d
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-11-15 15:50:42 +0200

    Complete week 2 exercise 12

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,16 +1,12 @@
 from kauppa import Kauppa
-from kirjanpito import Kirjanpito
+from kirjanpito import kirjanpito
 from varasto import Varasto
 from pankki import Pankki
 from viitegeneraattori import Viitegeneraattori
 
 
 def main():
-    viitegeneraattori = Viitegeneraattori()
-    kirjanpito = Kirjanpito()
-    varasto = Varasto(kirjanpito)
-    pankki = Pankki(kirjanpito)
-    kauppa = Kauppa(varasto, pankki, viitegeneraattori)
+    kauppa = Kauppa()
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
