9b0f8bd50199aff04df61029a3365c37f60f2994
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-11-16 12:18:53 +0200

    Print only Finnish players, complete week 3 exercise 1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,27 +1,29 @@
 import requests
 from player import Player
+from datetime import date, datetime
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
 
     players = []
 
     for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['team'],
-            player_dict['games'],
-            player_dict['goals'],
-            player_dict['assists']
-        )
+        if player_dict['nationality'] == "FIN":
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['games'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
 
-        players.append(player)
+            players.append(player)
 
-    print("Oliot:")
+    print("Players from Finland", datetime.now().strftime("%d.%m.%Y %H:%M"))
 
     for player in players:
         print(player)
