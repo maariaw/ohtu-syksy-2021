e6e4146deb97bb22decf3941c63f749521729d59
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-11-29 23:17:18 +0200

    Add test for basket having ostos with the right parameters after adding same item twice (step 12, accidentally completed code already)

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -84,3 +84,13 @@ class TestOstoskori(unittest.TestCase):
 
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    # step 11
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_ostosoliolla_tuotteen_nimi_ja_lukumaara_2(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
