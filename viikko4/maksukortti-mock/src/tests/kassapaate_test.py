fe944d611fea6a2b1d4f09fa52fa9f41bfcf6ca6
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-11-29 17:50:11 +0200

    Add tests and fix method lataa

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_korttia_ladataan_jos_lataussumma_positiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 12)
+
+        maksukortti_mock.lataa.assert_called_with(12)
+
+    def test_korttia_ei_ladata_jos_lataussumma_negatiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -20)
+
+        maksukortti_mock.lataa.assert_not_called()
