78a8dd2dd2e012eecef8a5a21e4ea1c4b2efe109
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-06 01:54:20 +0200

    Move scoring for advantage or win to its own method

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -18,16 +18,8 @@ class TennisGame:
         if self.player1_score == self.player2_score:
             score = self.get_even_score(self.player1_score)
         elif self.player1_score >= 4 or self.player2_score >= 4:
-            minus_result = self.player1_score - self.player2_score
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score = self.get_four_point_score(
+                self.player1_score - self.player2_score)
         else:
             for i in range(1, 3):
                 if i == 1:
@@ -60,3 +52,15 @@ class TennisGame:
             return scores[on_score]
 
         return "Deuce"
+
+    @staticmethod
+    def get_four_point_score(minus_result):
+        if minus_result == 1:
+            score = "Advantage player1"
+        elif minus_result == -1:
+            score = "Advantage player2"
+        elif minus_result >= 2:
+            score = "Win for player1"
+        else:
+            score = "Win for player2"
+        return score
