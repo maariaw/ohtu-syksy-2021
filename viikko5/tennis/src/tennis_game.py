61b29d033bcce8f6a3d71521c22b3e8962fb1b83
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-06 02:16:33 +0200

    Combine the even score and lower score methods

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -13,32 +13,28 @@ class TennisGame:
 
     def get_score(self):
         score = ""
-        temp_score = 0
 
-        if self.player1_score == self.player2_score:
-            score = self.get_even_score(self.player1_score)
-        elif self.player1_score >= 4 or self.player2_score >= 4:
+        if self.player1_score == self.player2_score or max(self.player1_score, self.player2_score) < 4:
+            score = self.get_lower_score(self.player1_score, self.player2_score)
+        else:
             score = self.get_four_point_score(
                 self.player1_score - self.player2_score)
-        else:
-            score = self.get_differing_score(
-                self.player1_score, self.player2_score)
 
         return score
 
-    @staticmethod
-    def get_even_score(on_score):
-        scores = {
-            0: "Love-All",
-            1: "Fifteen-All",
-            2: "Thirty-All",
-            3: "Forty-All"
-        }
+    # @staticmethod
+    # def get_even_score(on_score):
+    #     scores = {
+    #         0: "Love-All",
+    #         1: "Fifteen-All",
+    #         2: "Thirty-All",
+    #         3: "Forty-All"
+    #     }
 
-        if on_score in scores:
-            return scores[on_score]
+    #     if on_score in scores:
+    #         return scores[on_score]
 
-        return "Deuce"
+    #     return "Deuce"
 
     @staticmethod
     def get_four_point_score(minus_result):
@@ -53,11 +49,16 @@ class TennisGame:
         return score
 
     @staticmethod
-    def get_differing_score(first_score, second_score):
+    def get_lower_score(first_score, second_score):
         scores = {
             0: "Love",
             1: "Fifteen",
             2: "Thirty",
             3: "Forty"
         }
+        if first_score == second_score:
+            if first_score == 4:
+                return "Deuce"
+            else:
+                return scores[first_score] + "-All"
         return scores[first_score] + "-" + scores[second_score]
