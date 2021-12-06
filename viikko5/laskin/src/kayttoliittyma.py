138c384e290f47690dd1aceb952817acfba98847
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-06 04:31:02 +0200

    Refactor back to separate methods for Summa and Erotus, make Kumoa do canceling

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -15,8 +15,8 @@ class Kayttoliittyma:
         self._root = root
 
         self._komennot = {
-            Komento.SUMMA: Lasku(sovellus.plus, self._lue_syote),
-            Komento.EROTUS: Lasku(sovellus.miinus, self._lue_syote),
+            Komento.SUMMA: Summa(sovellus, self._lue_syote),
+            Komento.EROTUS: Erotus(sovellus, self._lue_syote),
             Komento.NOLLAUS: Nollaus(sovellus, self._lue_syote),
             Komento.KUMOA: Kumoa(sovellus, self._lue_syote)
         }
@@ -62,11 +62,16 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _lue_syote(self):
-        return self._syote_kentta.get()
+        try:
+            arvo = int(self._syote_kentta.get())
+        except Exception:
+            arvo = 0
+        return arvo
 
     def _suorita_komento(self, komento):
         komento_olio = self._komennot[komento]
         komento_olio.suorita()
+
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovellus.tulos == 0:
@@ -77,18 +82,21 @@ class Kayttoliittyma:
         self._syote_kentta.delete(0, constants.END)
         self._tulos_var.set(self._sovellus.tulos)
 
-class Lasku:
-    def __init__(self, laskutoimitus, lue_arvo):
-        self.laskutoimitus = laskutoimitus
+class Summa:
+    def __init__(self, sovellus, lue_arvo):
+        self.sovellus = sovellus
         self.lue_arvo = lue_arvo
 
     def suorita(self):
-        arvo = 0
-        try:
-            arvo = int(self.lue_arvo())
-        except Exception:
-            pass
-        self.laskutoimitus(int(arvo))
+        self.sovellus.plus(int(self.lue_arvo()))
+
+class Erotus:
+    def __init__(self, sovellus, lue_arvo):
+        self.sovellus = sovellus
+        self.lue_arvo = lue_arvo
+
+    def suorita(self):
+        self.sovellus.miinus(int(self.lue_arvo()))
 
 class Nollaus:
     def __init__(self, sovellus, lue_arvo):
@@ -104,4 +112,4 @@ class Kumoa:
         self.lue_arvo = lue_arvo
 
     def suorita(self):
-        pass
+        self.sovellus.aseta_arvo(self.sovellus.edellinen_arvo)
--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,11 +1,14 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.edellinen_arvo = tulos
 
     def miinus(self, arvo):
+        self.edellinen_arvo = self.tulos
         self.tulos = self.tulos - arvo
 
     def plus(self, arvo):
+        self.edellinen_arvo = self.tulos
         self.tulos = self.tulos + arvo
 
     def nollaa(self):
